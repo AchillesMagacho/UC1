208424103b32ebd9cf7e76b621b92a56eff5ece3
Achilles.Magacho <achillesmagach@gmail.com>
2026-07-06 20:44:23 -0300

    prog25.py alt

--- prog25.py
+++ prog25.py
@@ -1,8 +1,8 @@
 n1 = int(input("Digite 1 número :  "))
 n2 = int(input("Digite o segundo número :  "))
-if n1 > n2 
+if n1 > n2:
     print(f"O maior valor é {n1}")
-elif  n1 < n2 
+elif  n1 < n2:
     print(f"O maior valor é {n2}")
 else:
     print("os valores são iguais ou incompativeis")
